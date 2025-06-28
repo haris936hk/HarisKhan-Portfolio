60e1b3e6eaf7ce077033425bf71ea1540b49f89d
Haris Khan <hariskhan936.hk@gmail.com>
2025-06-29 01:17:34 +0500

    New Concept

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Open Source project",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Text Cipher Application",
+    des: " A modern Python GUI app for encrypting and decrypting text using various ciphers. Real-time decryption, light/dark mode, tabbed UI, input validation.",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Weather Monitoring System",
+    des: "A Java Swing application demonstrating the Observer Design Pattern with real-time weather updates via OpenWeatherMap APIs.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Task & Performance Management System",
+    des: "Designed to streamline task workflows, performance tracking, and employee development within organizations..",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Cognitive Assessment Games Suite",
+    des: "A suite of engaging games designed to assess cognitive abilities and promote mental agility.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Haris was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Haris's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Haris is the ideal partner.",
+    name: "Khalid Khan",
+    title: "CEO of SAMHAN Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Haris was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Haris's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Haris is the ideal partner.",
+    name: "Khalid Khan",
+    title: "CEO of SAMHAN Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Haris was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Haris's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Haris is the ideal partner.",
+    name: "Khalid Khan",
+    title: "CEO of SAMHAN Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Haris was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Haris's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Haris is the ideal partner.",
+    name: "Khalid Khan",
+    title: "CEO of SAMHAN Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Haris was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Haris's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Haris is the ideal partner.",
+    name: "Khalid Khan",
+    title: "CEO of SAMHAN Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern - PTCL",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Machine Learning Intern - Inotech",
+    desc: "Designed and developed ChatBot for both iOS & Android platforms using Chatbase.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Full Stack Intern - SAMHAN",
+    desc: "Developed a website for task and employee management, from initial concept to deployment in the organization.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
